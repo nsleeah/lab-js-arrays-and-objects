e22c0e4cbb9cca9527916f2e18953623a3fb547c
Leahnicoles <lnsimonn@hotmail.com>
2023-02-27 18:18:36 +0000

    add completed advanced exercise

--- advanced.js
+++ advanced.js
@@ -13,6 +13,7 @@ unitedKingdom = [
     name: "England",
     capital: "London",
     population: 55980000,
+    touristAttractions: ["The O2"],
   },
   {
     name: "Wales",
@@ -31,27 +32,45 @@ unitedKingdom = [
 /**
  * Q1. Add some tourist attractions to England
  */
-
-england = null;
-
-// console.log(england);
+//england = unitedKingdom [1].touristAttractions;
+//console.log(england);
 
 /**
  * Q2. Change the capital of Wales to "Cardiff"
  */
+//wales = unitedKingdom [2].capital = "Cardiff";
+//console.log(wales);
 
-wales = null;
-
-// console.log(wales);
 
 /**
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
-
-northernIrelandKeys = null;
-
-// console.log(northernIrelandKeys);
+//northernIrelandKeys = Object.keys(unitedKingdom[3]);
+//console.log(northernIrelandKeys);
 
 /**
  * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, smallest or somewhere in the middle.
  */
+if (unitedKingdom[0].population === unitedKingdom[1].population ) {
+  console.log("Somewhere in the middle");
+} else if (unitedKingdom[0].population >  unitedKingdom[1].population) {
+ console.log("Scotland is bigger than England");
+} else {
+  console.log("Scotland is smaller than England");
+}
+
+if (unitedKingdom[0].population === unitedKingdom[2].population ) {
+  console.log("Somewhere in the middle");
+} else if (unitedKingdom[0].population >  unitedKingdom[2].population) {
+ console.log("Scotland is bigger than Wales");
+} else {
+  console.log("Scotland is smaller than Wales");
+}
+
+if (unitedKingdom[0].population === unitedKingdom[3].population ) {
+  console.log("Somewhere in the middle");
+} else if (unitedKingdom[0].population >  unitedKingdom[3].population) {
+ console.log("Scotland is bigger than Northen Ireland");
+} else {
+  console.log("Scotland is smaller than Northern Ireland");
+}
